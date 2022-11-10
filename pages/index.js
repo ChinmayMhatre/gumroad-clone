3d1bf0277f2fd24361d1c08c2e11025b3c704e40
chinmay <chinmaymhatre12@gmail.com>
2022-11-10 16:43:20 +0530

    responsive navigation

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,13 @@ export default function Home() {
   const [displayLogo, setDisplayLogo] = useState(true)
   const [offSet, setOffSet] = useState(null)
   const imgRef = useRef(null)
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+      setIsOpen(!isOpen);
+  };
 
   const setScroll = () => {
-    console.log(imgRef.current.getBoundingClientRect().bottom);
     if (window.innerWidth < 1024) {
       setDisplayLogo(true)
       return
@@ -45,26 +49,45 @@ export default function Home() {
             ref={imgRef}
           />
         </div>
-        <div className="flex justify-between items-center sticky top-0 pl-[2rem] bg-white border-b-2 border-b-black">
-          <div className={`${displayLogo?'block':'hidden'}`}>
+        <div className={`flex justify-between items-center sticky top-0 pl-[2rem] bg-white border-b-2 border-b-black ${isOpen?'nav active':''}`}>
+          <div className={`${displayLogo?'block':'hidden'}  `} >
           <img
             src="/gumroad-logo.svg"
             width="100%"
           />
           </div>
-          <div className=" gap-10 flex font-semibold">
+          <div className=" gap-10 font-semibold hidden lg:flex">
             <div className='nav-link'>Features</div>
             <div className='nav-link'>Pricing</div>
             <div className='nav-link'>University</div>
             <div className='nav-link'>Blog</div>
             <div className='nav-link'>Discover</div>
           </div>
-          <div className=" flex font-semibold">
+          <div className=" lg:flex font-semibold hidden">
                 <div className=" px-[3vw] py-[1.5rem] border-l-2 border-black hover:bg-[#ff90e8]">Login</div>
                 <div className=" px-[3vw] py-[1.5rem] text-white bg-black hover:text-black hover:bg-[#ff90e8] border-l-2 border-black">Start Selling</div>
           </div>
+          <button onClick={toggleMenu} className={`burger ${isOpen?'active':''} transition-all duration-200 lg:hidden p-[1.5rem]`}>
+            <div className="bar transition-all duration-200 w-8 h-1 bg-black"></div>
+            <div className="bar transition-all duration-200 w-8 h-1 mt-2 bg-black"></div>
+            <div className="bar transition-all duration-200 w-8 h-1 mt-2 bg-black"></div>
+          </button>
         </div>
-        <div className="h-screen">sad</div>
+          <div className={`menu lg:hidden fixed h-screen -z-10 bg-black w-screen top-10 ${
+                    isOpen ? "active" : ""
+                }`}>
+jkl;kj
+        <div className="h-screen px-10 lg:hidden">
+          <div className="text-white text-lg font-bold hover:text-[#ff90e8] py-6">Login</div>
+          <div className="text-white text-lg font-bold hover:text-[#ff90e8] py-6">Start Selling</div>
+          <hr/>
+          <div className="text-white text-lg font-bold hover:text-[#ff90e8] py-6">Features</div>
+          <div className="text-white text-lg font-bold hover:text-[#ff90e8] py-6">Pricing</div>
+          <div className="text-white text-lg font-bold hover:text-[#ff90e8] py-6">University</div>
+          <div className="text-white text-lg font-bold hover:text-[#ff90e8] py-6">Blog</div>
+          <div className="text-white text-lg font-bold hover:text-[#ff90e8] py-6">Discover</div>
+        </div>
+          </div>
       </main>
 
     </div>
